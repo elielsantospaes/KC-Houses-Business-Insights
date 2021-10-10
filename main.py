548254bc73f4b1bd6a94b2d4fad22164cdad3676
eliel <elielriey@gmail.com>
2021-10-10 08:54:31 -0300

    Conslusao das verificaçoes das hipoteses. Inicio do relatorio

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# Projeto de portfólio com da base de dados da House Rocket
 import pandas as pd
 from pandas._config.config import set_option
 import numpy as np
@@ -9,8 +8,26 @@ import folium
 from folium.plugins import MarkerCluster
 from streamlit_folium import folium_static
 import statsmodels
+from math import sin, cos, sqrt, atan2, radians
 st.set_page_config(layout = 'wide')
 
+def calculate_distance(lat1, lon1, lat2, lon2):
+    # approximate radius of earth in m
+    R = 6371000.0
+
+    lat1 = radians(lat1)
+    lon1 = radians(lon1)
+    lat2 = radians(lat2)
+    lon2 = radians(lon2)
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    distance = R * c
+    return distance
 
 # 1 - Questões de negócio.
     # 1 - Quais os imóveis que a House Rocket deveria comprar e por qual preço?
@@ -23,18 +40,12 @@ st.set_page_config(layout = 'wide')
     #         - Relatório com as sugestões de compra de imóveis por um valor recomendado.
     #         - Relatório com as sugestões de venda de uma imóvel por um valor recomendado.
 
-    # 2 - Ferramentas 
-    #     Python
-    #     VS Code
-    #     Jupyter Notebook
         
-
 # 3 - Processo (Quais os passos necessários para alcançar meu objetivo)
 
     # 1 - Quais são os imóveis que a House Rocket deveria comprar?
         # - Coleta de dados (no Kaggle)
-st.title('House Rocket Project')
-st.write("The aim of the project is to define the best transactions opportunities within the House Rocket houses protifolium. ")
+
 def get_data(path):
     data = pd.read_csv(path)
     data['date'] = pd.to_datetime(data['date'])
@@ -44,19 +55,9 @@ def get_data(path):
 # Data Overview
 def show_data(data):
     st.header('Data Overview')
-    st.write(data.shape)
     st.write(data.head())
+    st.write('The data presents 21k houses and each house has 21 features')
 
-        # - Agrupar os dados por região (zipcode)
-        # - Dentro da cada região, eu vou encontrar a mediana do preço dos imóveis.
-        # - Vou sugerir que os imóveis que estão abaixo do preço mediano de dada região se que estejam em boas condições, sejam comprados.
-        
-        # - Exemplo 
-        #     Imóvel Cod | Região  | Preço do Imóvel | Preço da Mediana | Condições | Status
-        #        103131  | 3021346 | R$ 451201       | R$ 251354        | 3         | Não Compra
-        #        103131  | 3021346 | R$ 151201       | R$ 251354        | 3         | Compra
-        #        103131  | 3021346 | R$ 151201       | R$ 251354        | 1         | Não Compra
-  
 def remove_outliers(data, column):
     # Sort data by column
     #data.sort_values(column, inplace= True)
@@ -76,8 +77,6 @@ def remove_outliers(data, column):
     
     data.reset_index(inplace= True)
     data.drop(columns = 'index', axis = 1, inplace= True)
-    st.write(data.shape)
-    st.write(data.head())
     return data
 
 def mediam_price_per_condition(data):
@@ -92,7 +91,7 @@ def mediam_price_per_condition(data):
     fig = px.line(price_per_condition, x = 'condition', y = 'median_price')
     st.plotly_chart(fig)
 
-    st.write("As wee can see, the house's price increases with the increse of the house condition. So, the data will be grouped by region (zipcode) and condition.")
+    st.write("As wee can see, the house's price increases with the increase of the number that represents the house condition, indicating that the highest number the better house condition. So, the data will be grouped by region (zip code) and condition.")
 
 
 def median_price_per_condition_and_zipcode(data):
@@ -102,8 +101,8 @@ def median_price_per_condition_and_zipcode(data):
     
 def define_status(data, data_zip):
     st.header('Defining new features')
-    st.write("Using the grouped data by condidiotn and regions, lets define two new features for the houses:")
-    st.write(" - Status, that defines if a house should be bought")
+    st.write("Using the grouped data by condition and regions, lets define two new features for the houses:")
+    st.write(" - Status, that defines if a house should be bought or not. One house should be bought if the house price is lower than the median price for houses in the same condition and region.")
     st.write(" - x% lower, that shows how much the house's price is lower the than median price for a given condition and region.")
     
     data['median_price'] = 0
@@ -122,8 +121,8 @@ def define_status(data, data_zip):
     st.write(data[['price','condition', 'median_price', 'status','x% lower']].head(10))
     
 def best_opportunities(data):
-    st.header('Mapping best opportunities.')
-    st.write('Applying a filter in the data, takin only houses with status equal buy, lets see the distribution of opportunities, and how these opportunities are the best.')
+    st.header('Mapping the best opportunities.')
+    st.write('Applying a filter in the data, taken only houses with status equal buy, lets see the distribution of opportunities.')
     best = []
     for i in range(len(data)):
         if data.loc[i, 'x% lower'] > 0:
@@ -141,38 +140,41 @@ def best_opportunities(data):
         st.header('Statistical description of opportunities')
         st.write(best_df.describe())
 
-    st.write('By the chart and table we can conclude:')
+    st.write('By the chart and the table we can conclude:')
     st.write('')
-    st.write(" - There are 10678 houses with price lower than the median prices, in total")
-    st.write(" - For these 10k houses, in average, the prices are about 20% lower than the median prices.")
-    st.write(" - 25% of the houses are considered best opportunities, with prices equal or lower than 20% of the median prices.")
-    st.write(" - 75% of the houses are with discount lower than 20% and can generate a higher number of profitable transaction.")
+    st.write(" - There are 10113 houses with price lower than the median prices, in total.")
+    st.write(" - For these 10113 houses, in average, the prices are about 19% lower than the median prices.")
+    st.write(" - 25% of the houses are considered best opportunities, with prices equal or lower than 27% of the median prices.")
     st.write('')
     st.write('Based in the above conclusions, the data status will gain a new definition as follow:')
-    st.write(" - If the discont is higher than 20% the status will be changed to Buy_SRP, that means Strongly Recommended Purchase")
+    st.write(" - If the house price is 27% lower than the median price, the status will be changed to Buy_SRP, that means Strongly Recommended Purchase.")
     
-    for i in range(len(data) - 20000): # In the production the 20000 must be deleted.
-        if (data.loc[i, 'x% lower'] > 20):
+    for i in range(len(data)):
+        if (data.loc[i, 'x% lower'] > 27):
             data.loc[i, 'status'] = 'Buy_SRP'
     
     st.header('Date overview with new definition')
 
     st.write(data[['price','condition', 'median_price', 'status','x% lower']].head(10))
 
+    data['profit'] = np.nan
+    data.loc[data['status'] != 'Not Buy', 'profit'] = 100*(data['median_price']/data['price'] - 1)
+
+    st.header('Profit Evaluation')
+    st.write('Now, estimate the profit, considering the houses should be bought by the median price')
+    st.write(' - The averaged profit for the best opportunitires is: %.2f' %data.loc[data['status'] == 'Buy_SRP', 'profit'].mean())
+    st.write(' - The averaged profit for the commum opportunitires is: %.2f' %data.loc[data['status'] == 'Buy', 'profit'].mean())
+
+
+def house_sold(data):
+    pass
 
 def set_of_hypothesis(data):
     st.header('Set of Hypothesis')
     st.write('Now, lest check a set of hypothesis and verify if there are specifics opportunities. All the hypothesis were formulated based on the house features. Regions caracteristics were not considered.')
 
-# Hipóteses:
-    #     É interassante que as hipóteses sejam formuladas após conhecer os atributos dos imóveis.
-    #     Toda Hipótese de negócio precisa ter três características:
-    #         1 - precisa ser uma afirmação
-    #         2 - precisa ser uma comparação entre duas variáveis
-    #         3 - precisa ter um valor definido
-
-    # #     H1 - Imóveis que possuem vista para água são 20% mais caros, na média.
-    # st.write('H1 - Houses with waterfront are, in average, 20% more expensive.')
+    # H1 = 'Houses with waterfront are, in average, 20% more expensive.'
+    # st.write('H1' + H1)
 
     # # Select data where the houses have waterfront
     # # Take the averaged price per zipcode and condition
@@ -196,6 +198,7 @@ def set_of_hypothesis(data):
     # avg_price_ratio.dropna(axis = 0, inplace= True)
     # st.write(" - Value found: %.2f" % avg_price_ratio['avg_price_ratio'].mean(),'%')
     # st.write(" - H1 confirmed")
+    # st.write('Recomendation: ')
     # avg_price.clear()
 
     # st.write('Actilly the prices of houses with waterfront are, in the average, about 87% higher than prices of houses without waterfront.')
@@ -324,39 +327,53 @@ def set_of_hypothesis(data):
 
     # st.write('Houses with more than one bathrooms are, in average, about 43% more expensive')
 
-    #     H6 - Imóveis próximos da água e sem vista para a água são 20% mais baratos, na média.
-    st.write('H6 - houses near to water, but without waterfront, are, in average, 20% cheaper than houses with waterfront')
+    # #     H6 - Imóveis próximos da água e sem vista para a água são 20% mais baratos, na média.
+    # st.write('H6 - houses near to water, but without waterfront, are, in average, 20% cheaper than houses with waterfront')
 
-    # Select zipcode of houses with waterfront
-    # Select houses without waterfront
-    data_wf = data.loc[data['waterfront'] == 1, ['lat', 'long', 'condition', 'zipcode', 'price']]
-    data_wf.reset_index(inplace= True)
-
-    data_nwf = data.loc[(data['waterfront'] == 0) & (data['zipcode'].isin(data_wf['zipcode'].unique())), ['lat', 'long', 'condition', 'zipcode', 'price']]
-    data_nwf.reset_index(inplace= True)
-
-    waterfront_map = folium.Map(location = [data_wf['lat'].mean(), data_wf['long'].mean()], zoom_start = 10)
+    # # Select zipcode of houses with waterfront
+    # # Select houses without waterfront
+    # data_wf = data.loc[data['waterfront'] == 1, ['lat', 'long', 'condition', 'zipcode', 'price']]
+    # data_wf.reset_index(inplace = True)
+    # data_wf.drop(columns = 'index', inplace = True)
+        
+    # data_nwf = data.loc[(data['waterfront'] == 0) & (data['zipcode'].isin(data_wf['zipcode'].unique())) & (data['condition'].isin(data_wf['condition'].unique())), ['lat', 'long', 'condition', 'zipcode', 'price']]
+    # data_nwf.reset_index(inplace = True)
+    # data_nwf.drop(columns = 'index', inplace = True)
     
-    for i in range(len(data_wf)):
-        coordinate = [data_wf.loc[i, 'lat'], data_wf.loc[i, 'long']]
-        marker = folium.map.Marker(
-            coordinate,
-            # Create an icon as a text label
-            icon=folium.Icon(color='white', icon_color = 'green')           
-        )
-        waterfront_map.add_child(marker)
+    # for i in range(len(data_wf)):
+    #     for k in range(len(data_nwf)):
+    #         if(data_wf.loc[i, 'zipcode'] == data_nwf.loc[k, 'zipcode']):
+    #             if (calculate_distance(data_wf.loc[i, 'lat'], data_wf.loc[i, 'long'], data_nwf.loc[k, 'lat'], data_nwf.loc[k, 'long']) > 100):
+    #                 data_nwf.loc[k, 'zipcode'] = np.nan
 
-    for i in range(len(data_nwf)):
-        coordinate = [data_nwf.loc[i, 'lat'], data_nwf.loc[i, 'long']]
-        marker = folium.map.Marker(
-            coordinate,
-            # Create an icon as a text label
-            icon=folium.Icon(color='white', icon_color = 'blue')           
-        )
-        waterfront_map.add_child(marker)
+    # data_nwf.dropna(axis = 0, inplace= True)
+    # data_nwf.reset_index(inplace = True)
     
-    folium_static(waterfront_map)
+    # waterfront_map = folium.Map(location = [data_wf['lat'].mean(), data_wf['long'].mean()], zoom_start = 10)
+    
+    # for i in range(len(data_wf)):
+    #     coordinate = [data_wf.loc[i, 'lat'], data_wf.loc[i, 'long']]
+    #     marker = folium.map.Marker(
+    #         coordinate,
+    #         # Create an icon as a text label
+    #         icon=folium.Icon(color='white', icon_color = 'green')           
+    #     )
+    #     waterfront_map.add_child(marker)
 
+    # for i in range(len(data_nwf)):
+    #     coordinate = [data_nwf.loc[i, 'lat'], data_nwf.loc[i, 'long']]
+    #     marker = folium.map.Marker(
+    #         coordinate,
+    #         # Create an icon as a text label
+    #         icon=folium.Icon(color='white', icon_color = 'blue')           
+    #     )
+    #     waterfront_map.add_child(marker)
+    
+    # folium_static(waterfront_map)
+    
+    # st.write(" - There aren't houses that attends the hypothesis 6")
+    # st.write(" - H6 refuted")
+    # st.write('Recomendation: No recomendation')
 
     # #    H7 - Imóveis térreos são 20% mais caros, na média
     # st.write('H7 - Houses with only one floor are, in average, 20% more expensive. Due to people likes houses without laders')
@@ -493,6 +510,19 @@ def set_of_hypothesis(data):
 # Publicação do modelo
 
 if __name__ == '__main__':
+    st.title('House Rocket Project')
+
+    st.header('Abstract.')
+    st.write("The aim of the project is to define the best transactions opportunities within the House Rocket portfolio. To find what houses should be bought, two approach were took. First, a general comparison between houses with same condition and same region was done. With this approach the expected profit was, in average, XX%. The second approach takes the effects of the features of the houses to refine the analysis trying to find high proftable opportunities. Based in the houses features, 10 hypothesis were checked, and for the validateds hypothesis the expected profit, in average, was XX%. The best time to sold the houses were difined in the historical prices, considering the four year seasons. For houses near to water the summer time is the best time to sold, with increase of XX% in the profit in comparison with the same house sold in the winter time.")    
+    
+    st.header('Business understanding.')
+    st.write("For the portfolio analyzed in this work there are about 21k houses with ")
+
+
+    st.header('Premisses and assumptions')
+    st.write('1 - The price comparison will be made only for houses with same condition and in the same region.')
+    st.write('2 - The outliers will be removed, and will not be analzed.')
+    
     # Extraction
     path = 'dataset/kc_house_data.csv'
     data = get_data(path)
@@ -510,3 +540,9 @@ if __name__ == '__main__':
     best_opportunities(data)
 
     set_of_hypothesis(data)
+
+    # Load
+    #boy_repport(data)
+
+    #sold_repport(data)   
+    
